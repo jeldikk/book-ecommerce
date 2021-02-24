f29a6004d8f282ad42388d2daada5b89d7f09f69
jeldikk <jeldi.kamal2013@gmail.com>
2021-02-24 12:05:28 +0530

    Now dropdown will automatically close when clicked checkout button

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
 
+import {useHistory} from 'react-router-dom'
+
 import "./cart-dropdown.styles.scss"
+
+import {Button} from 'react-bootstrap'
 
 import {connect} from 'react-redux'
 
 import {calculateItemsCount} from "../../redux/cart/cart.utils"
+import {toggleCartDropDown} from "../../redux/cart/cart.actions"
 
-const CartDropdown = ({itemsCount}) => {
+const CartDropdown = ({itemsCount, toggleCartDropDown}) => {
+
+    const history = useHistory();
+
+    const onCheckoutClickHandler = (event)=>{
+        
+        history.push('/checkout');
+        toggleCartDropDown();
+    }
     return (
         <div className="cart-dropdown">
+            {
+                itemsCount === 0
+                ?
+                <div className="empty-cart-items">
+                    Your Cart is Empty. Add Items To checkout
+                </div>
+                :
+                <div className="cart-items-list">
+
+                </div>
+            }
+            <div className="button-div">
+                <Button variant="success" disabled={itemsCount === 0 ? true : false} onClick={onCheckoutClickHandler}>Checkout Items</Button>
+            </div>
             
         </div>
     )
@@ -20,4 +47,10 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps, null)(CartDropdown);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        toggleCartDropDown: () => dispatch(toggleCartDropDown())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartDropdown);
