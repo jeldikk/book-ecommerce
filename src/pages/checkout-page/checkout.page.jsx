37b5bd17c2c8149e9300c574c15bed008ec348c4
jeldikk <jeldi.kamal2013@gmail.com>
2021-02-24 12:04:32 +0530

    Added checkout page with sass style file

---
+++ src/pages/checkout-page/checkout.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./checkout-page.styles.scss"
+
+const Checkoutpage = () => {
+    return (
+        <div className="checkout-page">
+            Here is the checkout page code
+        </div>
+    )
+}
+
+export default Checkoutpage
