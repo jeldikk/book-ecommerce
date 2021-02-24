153472037f566a05915a536c998372764074b331
jeldikk <jeldi.kamal2013@gmail.com>
2021-02-24 10:31:44 +0530

    Added homepage component which routes to root path(/)

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import "./homepage.styles.scss"
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            This is just a homepage,
+            this page contains basic introduction on how to use applciation and different features implemented in it
+            <br />
+            Proper designing and laying out of this page is yet to be decided.
+            <br />
+            <br />
+
+        </div>
+    )
+}
+
+export default Homepage
