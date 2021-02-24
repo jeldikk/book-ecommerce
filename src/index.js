017018053b334bbb2ef778f800f58cbb72069705
jeldikk <jeldi.kamal2013@gmail.com>
2021-02-24 10:40:40 +0530

    Added redux Provider with configured store as prop over the <App /> component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+
+import {BrowserRouter as Router} from 'react-router-dom'
+import {Provider} from 'react-redux'
+
+import {store} from "./redux/store"
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
