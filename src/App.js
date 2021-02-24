d7ce5b99b6afafc8e5f60f17a2f8f669a7442660
jeldikk <jeldi.kamal2013@gmail.com>
2021-02-24 12:04:01 +0530

    Added route to /checkout and it will render Checkoutpage component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from "./components/footer/footer.component"
 
 import Homepage from "./pages/homepage/homepage.component"
 import Bookspage from "./pages/books-page/books-page.component"
+import Checkoutpage from "./pages/checkout-page/checkout.page.jsx"
 
 function App() {
   
@@ -18,6 +19,7 @@ function App() {
       <main>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/books" component={Bookspage} />
+        <Route exact path="/checkout" component={Checkoutpage} />
       </main>
       <Footer />
     </div>
