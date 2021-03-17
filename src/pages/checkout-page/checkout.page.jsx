05c3b908f2bf03054d4537d010b5a22a55711ccf
jeldikk <jeldi.kamal2013@gmail.com>
2021-03-17 12:45:20 +0530

    A page to render selected items using checkout-book-card component

--- src/pages/checkout-page/checkout.page.jsx
+++ src/pages/checkout-page/checkout.page.jsx
@@ -1,13 +1,51 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+
+import {connect} from 'react-redux'
+import {calculateItemsCount} from "../../redux/cart/cart.utils"
+import CheckoutBookCard from "../../components/checkout-book-card/checkout-book-card.component"
+
 
 import "./checkout-page.styles.scss"
 
-const Checkoutpage = () => {
+const Checkoutpage = ({cartItems, finalPay, itemsCount}) => {
     return (
         <div className="checkout-page">
-            Here is the checkout page code
+            <Container fluid="sm">
+                {
+                    itemsCount === 0 ?
+
+                    <div className="no-items-in-cart">
+                        No Items present In cart
+                    </div>
+                    :
+                    <>
+                        <div className="checkout-items">
+                            {
+                                cartItems.map((cartItem)=> <CheckoutBookCard key={cartItem.bookID} checkoutItem={cartItem} />)
+                            }
+                        </div>
+                        <div className="final-pay">
+                            {finalPay}
+                        </div>
+                    </>
+                }
+                
+                
+            </Container>
         </div>
     )
 }
 
-export default Checkoutpage
+
+const mapStateToProps = (state)=>{
+    return {
+        itemsCount: calculateItemsCount(state.cart.cartItems),
+        cartItems: state.cart.cartItems,
+        finalPay: state.cart.cartItems.reduce((accPay, item)=>{
+            return accPay + item.quantity*item.price
+        }, 0)
+    }
+}
+
+export default connect(mapStateToProps, null)(Checkoutpage)
