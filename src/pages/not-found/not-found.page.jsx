728768f875b09e5ce3d0e8964d77ee26ad6265d0
jeldikk <jeldi.kamal2013@gmail.com>
2021-03-20 17:12:12 +0530

    a not-found page to render on incorrect url

---
+++ src/pages/not-found/not-found.page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./not-found.styles.scss"
+
+const NotFoundUrl = () => {
+  return (
+    <div className="not-found">
+      <div className="title">URL not found</div>
+      <div className="description">
+        Reprehenderit nulla do labore excepteur deserunt. Duis consequat
+        consequat et irure dolore ipsum esse. Ex elit excepteur ad quis dolore
+        tempor ea nisi dolore. Qui occaecat Lorem nulla laboris magna dolore
+        quis sint qui proident et dolor Lorem.
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundUrl;
