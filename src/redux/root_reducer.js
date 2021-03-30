20cfdc28239887ab34ab1200f94b61ff757e9ba1
jeldikk <jeldi.kamal2013@gmail.com>
2021-03-30 18:13:46 +0530

    Added thunk to store middleware, included books redux property with necessary reducers and actions

--- src/redux/root_reducer.js
+++ src/redux/root_reducer.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux'
 
 import cartReducer from "./cart/cart.reducers"
+import booksReducer from "./books/books.reducers"
 
 const rootReducer = combineReducers({
-    cart: cartReducer
+    cart: cartReducer,
+    books: booksReducer
 })
 
 export default rootReducer
